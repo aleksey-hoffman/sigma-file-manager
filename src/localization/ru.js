df5e4f92d92a8c4b918597306225e206e48da697
Timofey Smolin <57951811+smotim@users.noreply.github.com>
2021-05-25 23:10:22 +0500

    Русская локализация
    Я еще не успел полностью изучить приложение, но оно мне уже очень нравится. Начал писать русский перевод. Не везде понял контекст, поэтому часть строчек недопереведены.
    
    I haven't fully explored the app yet, but I already really like it. I started writing a Russian translation. I didn't understand the context everywhere, so some of the lines are not translated correctly.

---
+++ src/localization/ru.js
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// License: GNU GPLv3 or later. See the license file in the project root for more information.
+// Copyright © 2021 - present Aleksey Hoffman. All rights reserved.
+
+const description = {
+  name: 'Русский',
+  locale: 'ru'
+}
+
+const data = {
+  app_name: 'Файловый менеджер Sigma',
+  locale_detected_message: 'Изменить язык приложения на русский?',
+  page_home_title: 'Домашняя страница',
+  page_navigator_title: 'Навигатор', 
+  page_dashboard_title: 'Закладки',
+  page_notes_title: 'Заметки',
+  page_settings_title: 'настройки',
+  settings_language_title: 'Язык',
+  settings_language_options_title: 'Язык приложения',
+  settings_ui_zoom_title: 'Масштабирование окна',
+  settings_ui_zoom_options_title: 'Уровень приближения элементов интерфейса | Текущий: ',
+  settings_ui_zoom_button_reset: 'Сбросить',
+  tooltip_button_shortcut: 'Горячая клавиша',
+  tooltip_button_ui_zoom_decrease_title: 'Для уменьшения уровеня приближения',
+  tooltip_button_ui_zoom_increase_title: 'Для увеличения уровеня приближения',
+  tooltip_button_ui_zoom_reset_title: 'Для сброса уровня приближения',
+  tooltip_button_full_screen_title: 'Полноэкранный режим',
+  tooltip_text_to_copy: 'To copy',
+  text_drive: 'Диск',
+  text_drives: 'Диски',
+  text_global_search_title: 'Глобальный поиск',
+  text_system_directories: 'Системные директории',
+  text_directory: 'директория',
+  text_file: 'файл',
+  text_file_symlink: 'file-symlink',
+  text_directory_symlink: 'directory-symlink',
+  text_item: 'item',
+  text_item_plural: 'items',
+  text_size: 'Размер',
+  text_file_size: 'Размер файла',
+  text_directory_size: 'Размер директории',
+  text_get_size: 'Get size',
+  text_path: 'Путь',
+  text_real_path: 'Link target path',
+  text_created: 'Created',
+  text_modified: 'Modified contents',
+  text_changed: 'Modified meta',
+  text_protected: 'Защищено',
+  text_protected_win32: 'Protected (within this app)',
+  text_pinned: 'Pinned',
+  text_mode: 'Разрешения',
+  text_yes: 'Да',
+  text_no: 'Нет',
+  text_read_only: 'Только для чтения',
+  text_undo: 'Отменить',
+  text_cancel: 'Отмена',
+  text_clear_list_message: 'item list has been cleared',
+  text_workspace_added: 'Рабочее пространство добавлено',
+  text_allow_global_search_message: 'Глобальный поиск позволяет быстро найти любой файл / директорию на вашем компьютере.<br>Он периодически сканирует ваши диски и генерирует файлы данных (карты), чтобы быстро находить ваши файлы.<br>Файлы, созданные после последнего сканирования, не будут найдены, так как их не будет в файлах данных, поэтому рекомендуется выбрать более частый период автоматического сканирования.'
+}
+
+export {
+  description,
+  data
+}
