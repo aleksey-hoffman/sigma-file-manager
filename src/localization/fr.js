32b085327c65c20c3182576a1ed5a2a111bb4583
CATHERINE Yohann <yohann.catherine@outlook.fr>
2021-05-30 16:16:17 +0200

    Added french language

---
+++ src/localization/fr.js
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// License: GNU GPLv3 or later. See the license file in the project root for more information.
+// Copyright © 2021 - present Aleksey Hoffman. All rights reserved.
+
+const description = {
+  name: 'Français',
+  locale: 'fr'
+}
+
+const data = {
+  app_name: 'Gestionnaire de fichier Sigma',
+  locale_detected_message: 'Changer la langue en français ?',
+  page_home_title: 'Accueil',
+  page_navigator_title: 'Navigation',
+  page_dashboard_title: 'Tableau de bord',
+  page_notes_title: 'Notes',
+  page_settings_title: 'paramètres',
+  settings_language_title: 'Langues',
+  settings_language_options_title: 'App language',
+  settings_ui_zoom_title: 'Mise à l\'échelle de la denêtre',
+  settings_ui_zoom_options_title: 'Niveau de zoom de l\'interface | Actuellement: ',
+  settings_ui_zoom_button_reset: 'Rétablir',
+  tooltip_button_shortcut: 'Raccourci',
+  tooltip_button_ui_zoom_decrease_title: 'Diminuer le niveau de zoom',
+  tooltip_button_ui_zoom_increase_title: 'Augmenter le niveau de zoom',
+  tooltip_button_ui_zoom_reset_title: 'Rétablir le niveau de zoom',
+  tooltip_button_full_screen_title: 'Plein écran',
+  tooltip_text_to_copy: 'Pour copier',
+  text_drive: 'Disque',
+  text_drives: 'Disques',
+  text_global_search_title: 'Recherche globale',
+  text_system_directories: 'Répertoire système',
+  text_directory: 'dossier',
+  text_file: 'fichier',
+  text_file_symlink: 'lien symbolique de fichier',
+  text_directory_symlink: 'lien symbolique de dossier',
+  text_item: 'éléments',
+  text_item_plural: 'éléments',
+  text_size: 'Taille',
+  text_file_size: 'Taille du fichier',
+  text_directory_size: 'Taille du dossier',
+  text_get_size: 'Connaître la taille',
+  text_path: 'Chemin d\'accès',
+  text_real_path: 'Link target path',
+  text_created: 'Date de création',
+  text_modified: 'Contenus modifiés',
+  text_changed: 'Modified meta',
+  text_protected: 'Protégé',
+  text_protected_win32: 'Protégé (dans cette application)',
+  text_pinned: 'Épinglé',
+  text_mode: 'Permissions',
+  text_yes: 'Oui',
+  text_no: 'Non',
+  text_read_only: 'Lecture seulement',
+  text_undo: 'annuler',
+  text_cancel: 'retour',
+  text_clear_list_message: 'la liste d\'élélement a été effacée',
+  text_workspace_added: 'l\'espace de travail a été ajouté',
+  text_allow_global_search_message: 'La recherche globale vous permet de trouver rapidement n\'importe quel fichier / dossier sur votre ordinateur<br>Il analyse régulièrement vos disques et génère des fichiers de données (cartes) afin de pouvoir retrouver rapidement vos fichiers<br>Les fichiers créés après la dernière analyse ne seront pas trouvés puisqu\'ils ne se trouvent pas dans les fichiers de données. Il est donc recommandé de choisir une analyse périodique, automatique et plus fréquente'
+}
+
+export {
+  description,
+  data
+}
