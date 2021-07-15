3784b9bb1d967cb8dcbb1dbcab6b2dcfaa490d81
jczanfona <josemariacasane@gmail.com>
2021-07-15 07:44:17 +0200

    Added_spanish_language

---
+++ src/localization/sp.js
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// License: GNU GPLv3 or later. See the license file in the project root for more information.
+// Copyright © 2021 - present Aleksey Hoffman. All rights reserved.
+
+const description = {
+  name: 'Spanish',
+  locale: 'sp'
+}
+
+const data = {
+  app_name: 'Sigma file manager',
+  locale_detected_message: '¿Cambiar el idioma de la aplicación al español?',
+  page_home_title: 'Página de inicio',
+  page_navigator_title: 'Navegador',
+  page_dashboard_title: 'Panel de Control',
+  page_notes_title: 'Notas',
+  page_settings_title: 'configuración',
+  settings_language_title: 'Idioma',
+  settings_language_options_title: 'Idioma de la App',
+  settings_ui_zoom_title: 'Ventana de zoom',
+  settings_ui_zoom_options_title: 'Nivel de zoom | Actual: ',
+  settings_ui_zoom_button_reset: 'Restablecer',
+  tooltip_button_shortcut: 'Tecla de acceso rápido',
+  tooltip_button_ui_zoom_decrease_title: 'Disminuir el nivel de zoom',
+  tooltip_button_ui_zoom_increase_title: 'Aumentar el nivel de zoom',
+  tooltip_button_ui_zoom_reset_title: 'Restablecer el nivel de zoom',
+  tooltip_button_full_screen_title: 'Modo de pantalla completa',
+  tooltip_text_to_copy: 'Para copiar',
+  text_drive: 'Disco',
+  text_drives: 'Discos',
+  text_global_search_title: 'Búsqueda global',
+  text_user_directories: 'Directorios del usuario',
+  text_directory: 'directorio',
+  text_file: 'archivo',
+  text_file_symlink: 'archivo-enlace simbólico',
+  text_directory_symlink: 'directorio-enlace simbólico',
+  text_item: 'elemento',
+  text_item_plural: 'elementos',
+  text_size: 'Tamaño',
+  text_file_size: 'Tamaño del archivo',
+  text_directory_size: 'Tamaño del directorio',
+  text_get_size: 'Obtener tamaño',
+  text_path: 'Ruta',
+  text_real_path: 'Enlace de la ruta de destino',
+  text_created: 'Creado',
+  text_modified: 'Contenido modificado',
+  text_changed: 'Meta modificado',
+  text_protected: 'Protegido',
+  text_protected_win32: 'Protegido (dentro de esta app)',
+  text_pinned: 'Fijado',
+  text_mode: 'Permisos',
+  text_yes: 'Sí',
+  text_no: 'No',
+  text_read_only: 'Solo lectura',
+  text_undo: 'deshacer',
+  text_cancel: 'cancelar',
+  text_clear_list_message: 'la lista de elementos ha sido borrada',
+  text_workspace_added: 'espacio de trabajo añadido',
+  text_allow_global_search_message: 'La búsqueda global le permite encontrar rápidamente cualquier archivo / directorio en su ordenador.<br>Escanea periódicamente sus discos y genera archivos de datos (mapas) para encontrar rápidamente sus archivos.<br>Los archivos que fueron creados desde el último escaneo no se encontrarán, ya que no estarán en los archivos de datos, por lo que se recomienda elegir un período de escaneo automático más frecuente.'
+}
+
+export {
+  description,
+  data
+}
