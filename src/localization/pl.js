01c010431224996e88779d5e6ec60fa02f0efdde
TheChilliPL <dragonacchilli@gmail.com>
2021-08-14 22:05:52 +0200

    Add Polish translation

---
+++ src/localization/pl.js
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// License: GNU GPLv3 or later. See the license file in the project root for more information.
+// Copyright © 2021 - present Aleksey Hoffman. All rights reserved.
+
+const description = {
+  name: 'Polski',
+  locale: 'pl'
+}
+
+const data = {
+  app_name: 'Menedżer plików Sigma',
+  locale_detected_message: 'Zmienić język aplikacji na polski?',
+  page_home_title: 'Strona główna',
+  page_navigator_title: 'Nawigator',
+  page_dashboard_title: 'Panel',
+  page_notes_title: 'Notatki',
+  page_settings_title: 'Ustawienia',
+  settings_language_title: 'Język',
+  settings_language_options_title: 'Język aplikacji',
+  settings_ui_zoom_title: 'Skalowanie okna',
+  settings_ui_zoom_options_title: 'Powiększenie interfejsu | Obecne: ',
+  settings_ui_zoom_button_reset: 'Resetuj',
+  tooltip_button_shortcut: 'Skrót',
+  tooltip_button_ui_zoom_decrease_title: 'Zmniejsz powiększenie',
+  tooltip_button_ui_zoom_increase_title: 'Zwiększ powiększenie',
+  tooltip_button_ui_zoom_reset_title: 'Zresetuj powiększenie',
+  tooltip_button_full_screen_title: 'Pełny ekran',
+  tooltip_text_to_copy: 'Do skopiowania',
+  text_drive: 'Dysk',
+  text_drives: 'Dyski',
+  text_global_search_title: 'Globalne wyszukiwanie',
+  text_user_directories: 'Katalogi użytkownika',
+  text_directory: 'katalog',
+  text_file: 'plik',
+  text_file_symlink: 'plik-symlink',
+  text_directory_symlink: 'katalog-symlink',
+  text_item: 'element',
+  text_item_plural: 'elementy',
+  text_size: 'Rozmiar',
+  text_file_size: 'Rozmiar pliku',
+  text_directory_size: 'Rozmiar katalogu',
+  text_get_size: 'Oblicz rozmiar',
+  text_path: 'Ścieżka',
+  text_real_path: 'Ścieżka docelowa',
+  text_created: 'Stworzono',
+  text_modified: 'Zmieniono zawartość',
+  text_changed: 'Zmieniono metadane',
+  text_protected: 'Chronione',
+  text_protected_win32: 'Chronione (w tej aplikacji)',
+  text_pinned: 'Przypięte',
+  text_mode: 'Uprawnienia',
+  text_yes: 'Tak',
+  text_no: 'Nie',
+  text_read_only: 'Tylko do odczytu',
+  text_undo: 'cofnij',
+  text_cancel: 'anuluj',
+  text_clear_list_message: 'lista elementów została wyczyszczona',
+  text_workspace_added: 'dodano nową przestrzeń roboczą',
+  text_allow_global_search_message: 'Globalne wyszukiwanie pozwala szybko znaleźć jakikolwiek plik / folder na tym komputerze.<br>Co jakiś czas skanuje twoje dyski i generuje pliki danych (mapy) aby móc szybko przeszukać twoje pliki.<br>Pliki stworzone po ostatnim skanie nie będą znalezione, bo nie będzie ich w plikach danych, więc zalecany jest wybór niewielkiego okresu skanowania.'
+}
+
+export {
+  description,
+  data
+}
