63fc1577b570ed44b2da38d89e0c85066c86f21a
Aleksey Hoffman <61761672+aleksey-hoffman@users.noreply.github.com>
2021-09-01 04:27:57 +0300

    dev: rm allowRendererProcessReuse; switch diskusage; sync package-lock
    Remove deprecated `electron.app.allowRendererProcessReuse` to prepare
    for Electron v14.0.0. Since I figured that the window reloading
    issue in Electron v14.0.0 (without allowRendererProcessReuse) was
    caused by `electron.BrowserWindow.getFocusedWindow().reload()`
    in main::disableAppMenu() which is not essential and can be removed.
    
    Switch to another diskusage module fork. Current one doesn't work on
    some Electron versions (e.g. 13.2.2)

--- src/electronMain.js
+++ src/electronMain.js
@@ -79,9 +79,6 @@ function lockSingleAppInstance () {
 }
 
 function setAppProperties () {
-  // Temporary override. The default value breaks window loading on reload (sometimes)
-  // See https://github.com/electron/electron/issues/30710
-  electron.app.allowRendererProcessReuse = false
   electron.app.setAppUserModelId('com.alekseyhoffman.sigma-file-manager')
 }
 
