d79b3a5bee2d3b6d17e5b7bbaff6cbb77e3f95c9
JaanDev <67502867+JaanDev@users.noreply.github.com>
2021-09-08 20:53:08 +0300

    Create ru.js

---
+++ src/localization/ru.js
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// License: GNU GPLv3 or later. See the license file in the project root for more information.
+// Copyright © 2021 - present Aleksey Hoffman. All rights reserved.
+
+const description = {
+  name: 'Русский',
+  locale: 'ru'
+}
+
+const data = {
+  app_name: 'Проводник Sigma',
+  locale_detected_message: 'Поменять язык приложения на русский?',
+  page_home_title: 'Домашняя страница',
+  page_navigator_title: 'Навигация',
+  page_dashboard_title: 'Быстрый доступ',
+  page_notes_title: 'Заметки',
+  page_settings_title: 'настройки',
+  settings_language_title: 'Язык',
+  settings_language_options_title: 'Язык приложения',
+  settings_ui_zoom_title: 'Масштаб приложения',
+  settings_ui_zoom_options_title: 'Масштаб интерфейса | Сейчас: ',
+  settings_ui_zoom_button_reset: 'Вернуть',
+  tooltip_button_shortcut: 'Ярлык',
+  tooltip_button_ui_zoom_decrease_title: 'Уменьшить масштаб',
+  tooltip_button_ui_zoom_increase_title: 'Увеличить масштаб',
+  tooltip_button_ui_zoom_reset_title: 'Вернуть масштаб',
+  tooltip_button_full_screen_title: 'Полноэкранный режим',
+  tooltip_text_to_copy: 'Копировать',
+  text_drive: 'Диск',
+  text_drives: 'Диски',
+  text_global_search_title: 'Глобальный поиск',
+  text_user_directories: 'Папки пользователя',
+  text_directory: 'папка',
+  text_file: 'файл',
+  text_file_symlink: 'ярлык файла',
+  text_directory_symlink: 'ярлык папки',
+  text_item: 'приедмет',
+  text_item_plural: 'предметы',
+  text_size: 'Размер',
+  text_file_size: 'Размер файла',
+  text_directory_size: 'Размер папки',
+  text_get_size: 'Вычислить размер',
+  text_path: 'Путь',
+  text_real_path: 'Конечный путь ярлыка',
+  text_created: 'Создано',
+  text_modified: 'Изменено',
+  text_changed: 'Изменены свойства',
+  text_protected: 'Защищено',
+  text_protected_win32: 'Защищено(в этом приложении)',
+  text_pinned: 'Закреплено',
+  text_mode: 'Разрешения',
+  text_yes: 'Да',
+  text_no: 'Нет',
+  text_read_only: 'Только для чтения',
+  text_undo: 'вернуть',
+  text_cancel: 'отмена',
+  text_clear_list_message: 'список предметов очищен',
+  text_workspace_added: 'добавлено рабочее пространство',
+  text_allow_global_search_message: 'Глобальный поиск позволяет быстро найти файл/папку на вашем устройстве.<br>Он периодически сканирует диски и создаёт файлы данных(карты), чтобы искать файлы быстрее.<br>Файлы, созданные после последнего сканирования не будут найдены, так как они не находятся в файлах данных, поэтому лучше выбирать более частый интервал сканирования.'
+}
+
+export {
+  description,
+  data
+}
