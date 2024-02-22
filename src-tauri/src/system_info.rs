34dc055e162903cea539398f2ba98f9b932a9f55
aleksey-hoffman <61761672+aleksey-hoffman@users.noreply.github.com>
2024-02-22 15:32:37 +0300

    Dev: refactor system info methods and types

--- src-tauri/src/system_info.rs
+++ src-tauri/src/system_info.rs
@@ -3,7 +3,7 @@
 // Copyright © 2021 - present Aleksey Hoffman. All rights reserved.
 
 use serde::Serialize;
-use sysinfo::{System, SystemExt};
+use sysinfo::System;
 
 #[derive(Clone, Serialize)]
 pub struct SystemStats {
@@ -12,17 +12,23 @@ pub struct SystemStats {
     host_name: String,
     kernel_version: String,
     os_version: String,
+    total_memory: u64,
+    available_memory: u64,
+    used_memory: u64,
 }
 
 lazy_static::lazy_static! {
   pub static ref SYSTEM_STATS: SystemStats = {
     let sys = System::new_all();
     SystemStats {
-      name: sys.name().unwrap_or_default(),
-      long_os_version: sys.long_os_version().unwrap_or_default(),
-      host_name: sys.host_name().unwrap_or_default(),
-      kernel_version: sys.kernel_version().unwrap_or_default(),
-      os_version: sys.os_version().unwrap_or_default(),
+      name: System::name().unwrap_or_default(),
+      long_os_version: System::long_os_version().unwrap_or_default(),
+      host_name: System::host_name().unwrap_or_default(),
+      kernel_version: System::kernel_version().unwrap_or_default(),
+      os_version: System::os_version().unwrap_or_default(),
+      total_memory: sys.total_memory(),
+      available_memory: sys.available_memory(),
+      used_memory: sys.used_memory(),
     }
   };
 }
