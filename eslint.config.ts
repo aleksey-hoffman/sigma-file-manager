e57c4cc1fd6146c25493711de272fbc0e2c2ec94
aleksey-hoffman <aleksey.hoffman.public@gmail.com>
2026-01-10 21:30:23 +0300

    Dev: remove oxlint

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,7 +2,6 @@ import { globalIgnores } from 'eslint/config';
 import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
 import pluginVue from 'eslint-plugin-vue';
 import pluginVitest from '@vitest/eslint-plugin';
-import pluginOxlint from 'eslint-plugin-oxlint';
 import pluginStylistic from '@stylistic/eslint-plugin';
 import checkFile from 'eslint-plugin-check-file';
 import pluginVueI18n from '@intlify/eslint-plugin-vue-i18n';
@@ -18,7 +17,6 @@ export default defineConfigWithVueTs(
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
   },
-  ...pluginOxlint.configs['flat/recommended'],
   {
     files: ['**/*.{ts,mts,tsx,vue}'],
     plugins: {
